1fb7e59a6af0a8c39cf76cb76ae8a122ab071d18
E-Knecht <142593414+E-Knecht@users.noreply.github.com>
2024-01-27 16:50:32 +0100

    Add files via upload

---
+++ vigenère-verschlüsselung WinForms/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vigenère_verschlüsselungO;
+
+namespace vigenère_verschlüsselung_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Verschluessung vigenère_verschlüsselung = new Verschluessung();
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            vigenère_verschlüsselung.key = KeyRichTextBox.Text.Replace(" ", ""); ;
+            vigenère_verschlüsselung.codedText = inputRichTextBox.Text.Replace(" ", ""); ;
+            OutputRichTextBox.Text = vigenère_verschlüsselung.clearText;
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            vigenère_verschlüsselung.key = KeyRichTextBox.Text;
+            vigenère_verschlüsselung.clearText = inputRichTextBox.Text;
+            OutputRichTextBox.Text = vigenère_verschlüsselung.codedText;
+        }
+
+        private void OutputRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ vigenère-verschlüsselung WinForms/Verschluessung.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace vigenère_verschlüsselungO
+{
+
+
+    public class Verschluessung
+    {
+
+        public string key
+        {
+            set
+            {
+                this.inputKeyInt = this.stringToNumberList(value);
+                if (_clearText != "")
+                    clearText = _clearText;
+                else if (_codedText != "")
+                    codedText = _codedText;
+            }
+        }
+        public string clearText
+        {
+            get
+            {
+                return _clearText;
+            }
+            set
+            {
+                _clearText = value;
+                _codedText = encrypt();
+            }
+        }
+        private string _clearText = "";
+        public string codedText
+        {
+            get
+            {
+                return _codedText;
+            }
+            set
+            {
+                _codedText = value;
+                _clearText = decrypt();
+            }
+        }
+
+        private string _codedText = "";
+
+        private List<int> inputKeyInt;
+        private static char[] alphabetToNummbre = Enumerable.Range('a', 26).Select(asciCode => (char)asciCode).ToArray();
+        private List<int> stringToNumberList(string inputString)
+        {
+            char[] inputList = (this.StringChar(inputString));
+            List<int> modifiedList = new List<int>();
+
+            for (int i = 0; i < inputList.Length; i++)
+            {
+                int I = alphabetToNummbre.Length;
+                while (inputList[i] != alphabetToNummbre[--I])
+                {
+                    if (I == 0)
+                        break;
+                }
+                modifiedList.Add(I);
+            }
+
+            return modifiedList;
+        }
+        private char[] StringChar(string inputstring)
+        {
+            char[] modifiedArray = new char[inputstring.Length];
+            for (int i = 0; i < inputstring.Length; i++)
+            {
+                if (inputstring[i] != ' ')
+                {
+                    modifiedArray[i] = inputstring.ToLower()[i];
+
+                    if (inputstring[i] > 128)
+                    {
+                        modifiedArray[i] = 'a';
+                    }
+                }
+            }
+            return modifiedArray;
+        }
+
+
+        private string encrypt()
+        {
+            return crypting(1, _clearText);
+        }
+        private string decrypt()
+        {
+            return crypting(-1, _codedText);
+        }
+        private string crypting(int sign, string transformString)
+        {
+            if ((inputKeyInt == null) || (inputKeyInt.Count == 0) || (transformString == ""))
+            {
+                return "";
+            }
+
+            List<int> transformCode = stringToNumberList(transformString);
+            List<int> outputList = new List<int>();
+            for (int i = 0; i < transformCode.Count; i++)
+            {
+                int outputCode = (transformCode[i] + sign * inputKeyInt[i % inputKeyInt.Count]) % alphabetToNummbre.Length;
+                outputList.Add((outputCode >= 0) ? outputCode : outputCode + alphabetToNummbre.Length);
+            }
+            char[] output = new char[outputList.Count];
+            for (int i = 0; i < outputList.Count; i++)
+            {
+                output[i] = alphabetToNummbre[outputList[i]];
+            }
+
+            return new string(output);
+        }
+    }
+}
